ad610e7a6935c6291ad0d36e608a58c93d96f3bf
ibanpare <13278805+ibanpare@users.noreply.github.com>
2023-03-14 20:05:26 +0100

    Add operate function

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a -b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a/b;
+
+function operate(a, b, operand) {
+    if (operand === "+") return add(a, b);
+    if (operand === "-") return subtract(a, b);
+    if (operand === "X") return multiply(a, b);
+    if (operand === "/") return divide(a, b);
+}
