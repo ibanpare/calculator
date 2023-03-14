e6a484c9dcb97889b090714de4165ebc54cc3bd7
ibanpare <13278805+ibanpare@users.noreply.github.com>
2023-03-14 20:35:40 +0100

    Add first display funcions

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+let displayValue = "Start typing";
+const display = document.querySelector("#display");
+display.textContent = displayValue;
+
+const buttons = document.querySelectorAll(".buttons div p");
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        updateDisplay(button.textContent);
+    });
+  });
+
 const add = (a, b) => a + b;
 const subtract = (a, b) => a -b;
 const multiply = (a, b) => a * b;
@@ -9,3 +21,13 @@ function operate(a, b, operand) {
     if (operand === "X") return multiply(a, b);
     if (operand === "/") return divide(a, b);
 }
+
+//updates the display, need to add clear logic
+function updateDisplay(content) {
+    displayValue = content;
+    display.textContent = displayValue;
+}
+
+
+
+
