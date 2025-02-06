cd06ccf5bfe89ef39ef8a8a19251cc7169bc5c5e
ibanpare <13278805+ibanpare@users.noreply.github.com>
2025-02-06 18:29:05 +0100

    refactor: add function for update screen and clean up£

--- script.js
+++ script.js
@@ -33,21 +33,41 @@ function operate(a, b, operator) {
     }
 }
 
+function updateScreen(content) {
+    if(screen.textContent.includes(".") && (content == ".")) {
+        screen.textContent = screen.textContent;
+    } else
+    {screen.textContent += content;}
+}
+
 function recordClickedDigits() {
     digits.forEach((button) => {
-        button.addEventListener("click", (e) => {
-            if(screen.textContent.includes(".") && (e.target.textContent == ".")) {screen.textContent = screen.textContent} else {
-            screen.textContent += e.target.textContent;}
-        })
+        button.addEventListener("click", (e) => updateScreen(e.target.textContent))
     })
 }
 
 recordClickedDigits();
 
 /* 
-da sistemare
+Make the calculator work! You’ll need to store the first and second numbers input by the user and then operate() on them when the user presses the = button, according to the operator that was selected between the numbers.
 
-puoi mettere due virgole e bisogna evitarlo
-css dello screen incluso flexbox
-move to step 6 of exercise after
+You should already have the code that can populate the display, so once operate has been called, update the display with the result of the operation.
+
+This is the hardest part of the project. You need to figure out how to store all the values and call the operate function with them. Don’t feel bad if it takes you a while to figure out the logic.
+
+
+SE CLICCHI UNO TRA +, -, ecc
+CONSERVA I CONTENUTI DELLO SCHERMO IN VARIABILE FIRST NUMBER
+CONSERVA +, -, ecc IN OPERATOR
+PULISCI SCHERMO
+CONTINUA A REGISTRARE DIGITS E RIEMPIRE SCHERMO
+FINCHE CLICCHI =
+LI SALVA SCHERMO IN SECOND NUMBER
+PULISCI SCHERMO
+CHIAMA FUNZIONE IN BASE A OPERATOR
+RIEMPI SCHERMO CON RISULTATO
+
+
+BISOGNA FARE REFACTOR DI RECORD CLICKED DIGITS e FARLO SU TUTTI I BOTTONI, CON VARI CASI
+AGGIORNANDO VARIABIL
 */
